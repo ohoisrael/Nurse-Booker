f16ae9128998d94d315f1199a1a6d0519f19457e
IsraelOho <151618401+ohoisrael@users.noreply.github.com>
2024-05-07 10:06:30 +0000

    Update config.js

--- Frontend/src/config.js
+++ Frontend/src/config.js
@@ -1,2 +1,2 @@
-export const BASE_URL = 'https://nurse-booker-3.onrender.com';
+export const BASE_URL = 'https://nurse-booker-3.onrender.com/api/v1';
 export const token = localStorage.getItem("token");
