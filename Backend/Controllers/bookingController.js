d172d91461bf3167e20d5d4a4ae75c0ac6114d36
IsraelOho <151618401+ohoisrael@users.noreply.github.com>
2024-06-02 10:52:55 +0000

    Update bookingController.js

--- Backend/Controllers/bookingController.js
+++ Backend/Controllers/bookingController.js
@@ -14,7 +14,7 @@ export const getCheckoutSession = async (req, res) => {
         const session = await stripe.checkout.sessions.create({
             payment_method_types: ['card'],
             mode: 'payment',
-            success_url: `${process.env.CLIENT_SITE_URL}/checkout-success`,
+            success_url: `${process.env.CLIENT_SITE_URL}checkout-success`,
             cancel_url: `${req.protocol}://${req.get('host')}/nurses/${nurse.id}`,
             customer_email: user.email,
             client_reference_id: req.params.nurseId,
